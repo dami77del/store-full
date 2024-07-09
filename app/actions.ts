829c2308fb51492844ba3f5cf57aaf3d86b61868
dami77del <ddeligdisch@gmail.com>
2024-07-09 21:37:11 +0200

    Enhance Dashboard with EditForm functionality

--- app/actions.ts
+++ app/actions.ts
@@ -20,7 +20,7 @@ export async function createProduct(prevState: unknown, formData: FormData) {
     }
 
     const flattenUrls = submission.value.images.flatMap((urlString)=>
-         urlString.split(",").map((url)=> url.trim()))
+         urlString.split(",").map((url)=> url.trim()));
  await prisma.product.create({
      data: {
        name:submission.value.name,
@@ -34,5 +34,42 @@ export async function createProduct(prevState: unknown, formData: FormData) {
      },
  })
  redirect("/dashboard/products")
+}
+
+export async function editProduct( prevState: unknown, formData : FormData){
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    if (!user || user.email !== 'damiandelok@gmail.com') {
+        return redirect("/")
+    }
+
+    const submission = parseWithZod(formData,{
+        schema: productSchema,
+    })
+
+    if (submission.status!== 'success') {
+        return submission.reply();       
+    }
+
+    const flattenUrls = submission.value.images.flatMap((urlString)=>
+        urlString.split(",").map((url)=> url.trim()));
+
+    const productId = formData.get("productId") as string
+    await prisma.product.update({
+        where:{
+            id: productId,
+        },
+        data:{
+            name: submission.value.name,
+            description: submission.value.description,
+            status: submission.value.status,
+            price: submission.value.price,
+            images: flattenUrls,
+            category: submission.value.category,
+            isFeatured: submission.value.isFeatured,
+        }
+    })
+
+    redirect("/dashboard/products");
 
 }
